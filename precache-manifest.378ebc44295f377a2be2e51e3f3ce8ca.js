2385a9b065ffb06bbcb77ea610d3f71e12243afc
Nicofru <n.fruneau@gmail.com>
2021-06-30 17:04:39 +0200

    Updates

---
+++ precache-manifest.378ebc44295f377a2be2e51e3f3ce8ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c556845ac5674f2d68079adce79fd36",
+    "url": "/TruffleSimpleStorage/index.html"
+  },
+  {
+    "revision": "334752cce97830f71eab",
+    "url": "/TruffleSimpleStorage/static/css/main.a3487775.chunk.css"
+  },
+  {
+    "revision": "528c4e0ce26b92599105",
+    "url": "/TruffleSimpleStorage/static/js/2.97793b9f.chunk.js"
+  },
+  {
+    "revision": "334752cce97830f71eab",
+    "url": "/TruffleSimpleStorage/static/js/main.44b80b97.chunk.js"
+  },
+  {
+    "revision": "3f52796d9cd50cb29840",
+    "url": "/TruffleSimpleStorage/static/js/runtime-main.051a6749.js"
+  }
+]);
